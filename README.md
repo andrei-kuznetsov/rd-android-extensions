b31b283286f3cf8e902873afde21d5c2b65c5f1d
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2021-12-22 12:53:37 +0300

    Add support for "Attach to Android Process" button on client side

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rd-android-extensions
 IDEA Extensions for Android Plugin in RemoteDev
+
+Build:
+```
+./gradlew clean buildPlugin
+```
+
+Output:
+```
+build/distributions/rd-android-extensions-<version>>.zip
+```
---
+++ src/main/java/org/intellij/android/cwm/AndroidUnattendedBackendDelegatingActionIdsProvider.kt
@@ -0,0 +1,39 @@
+package org.intellij.android.cwm
+
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
+import com.jetbrains.rd.actions.AccessRequirements
+import com.jetbrains.rd.ide.model.AddToGroupRuleModel
+import com.jetbrains.rd.platform.codeWithMe.permissions.RunAccessLevel
+import com.jetbrains.rdserver.actions.BackendDelegatingActionIdsProvider
+import com.jetbrains.rdserver.core.GuestProjectSession
+import com.jetbrains.rdserver.unattendedHost.UnattendedHostManager
+
+class AndroidUnattendedBackendDelegatingActionIdsProvider : BackendDelegatingActionIdsProvider {
+
+    init {
+        if (!UnattendedHostManager.getInstance().isUnattendedMode) {
+            throw ExtensionNotApplicableException.INSTANCE
+        }
+    }
+
+    private val UNATTENDED_ACTION_IDS = mapOf(
+        // Add explicitly because it contains word "Debug", which is excluded in DefaultBackendDelegatingActionIdsProvider
+        "AndroidConnectDebuggerAction" to AccessRequirements.of(RunAccessLevel.FullAccess)
+    )
+
+    override fun getAccessRequirements(actionId: String, session: GuestProjectSession): AccessRequirements? {
+        return UNATTENDED_ACTION_IDS[actionId]
+    }
+
+    override fun getActionIds(session: GuestProjectSession): Collection<String> {
+        return UNATTENDED_ACTION_IDS.keys
+    }
+
+    override fun getHintParentGroupIds(session: GuestProjectSession): Map<String, Collection<AddToGroupRuleModel>> {
+        return emptyMap()
+    }
+
+    override fun getEditorActionWithImmediateResultIds(session: GuestProjectSession): Collection<String> {
+        return emptyList()
+    }
+}
